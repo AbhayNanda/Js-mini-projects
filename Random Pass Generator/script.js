43b4204506ccd7edadee1fb130d1023040d7d279
AbhayNanda <abhayne14@gmail.com>
2024-03-05 18:47:07 +0530

    Adding JS to Password Generator

---
+++ Random Pass Generator/script.js
@@ -0,0 +1,29 @@
+const passwordbox = document.getElementById("password");
+const length = 12;
+
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+
+const allChars = uppercase + lowercase + number;
+
+function createpass() {
+  let password = "";
+  password += uppercase[Math.floor(Math.random() * uppercase.length)];
+  password += lowercase[Math.floor(Math.random() * lowercase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordbox.value = password;
+}
+
+function copypassword() {
+  const textarea = document.createElement('textarea');
+    textarea.value = text;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+}
